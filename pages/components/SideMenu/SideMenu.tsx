7e13bf3a411bd58581331d39335b11a3b70be36c
braincee <kwesi938@gmail.com>
2023-07-25 22:14:57 +0000

    Add SideMenu component

---
+++ pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../Layout'
+
+const SideMenu = () => {
+  return (
+    <Layout>
+    <ul>
+        <li>Profile</li>
+        <li>Account</li>
+        <li>Logout</li>
+    </ul>
+    </Layout>
+
+  )
+}
+
+export default SideMenu
