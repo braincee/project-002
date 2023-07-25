6a66cc033aa2fb903cf890458f6468f1b5af64f6
braincee <kwesi938@gmail.com>
2023-07-25 22:14:30 +0000

    Add Header component

---
+++ pages/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, NavbarContent, NavbarItem } from '@nextui-org/react';
+
+const Header = () => {
+
+    return (
+      <Navbar brand="NFT Gated Server">
+          <NavbarContent variant="highlight">
+            <NavbarItem >
+            </NavbarItem>
+            <NavbarItem className="h-full">
+             </NavbarItem>
+             <NavbarItem className="h-full">
+             </NavbarItem>
+          </NavbarContent>
+      </Navbar>
+    );
+  };
+  
+  export default Header;
+  
