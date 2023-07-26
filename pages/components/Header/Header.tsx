66b4d3094cfe87d3aeffbe03e500e0578834b64d
braincee <kwesi938@gmail.com>
2023-07-26 18:07:43 +0000

    Add dark and Light mode

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -6,12 +6,54 @@ import ButtonGroup from '@mui/joy/ButtonGroup';
 import Link from '@mui/joy/Link';
 import Box from '@mui/joy/Box';
 import { Typography } from '@mui/joy';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
 
+
+const useEnhancedEffect =
+  typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+
+function ModeSwitcher() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <Button
+      variant="outlined"
+      color="neutral"
+      onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+    >
+      {mode === 'dark' ? 'Turn light' : 'Turn dark'}
+    </Button>
+  );
+}
 
 
 const Header: React.FC = () => {
+
+  const [node, setNode] = React.useState<HTMLElement | null>(null);
+  useEnhancedEffect(() => {
+    setNode(document.getElementById('mode-toggle'));
+  }, []);
+
   return (
-    <Box sx={{ display: 'flex', gap: 55, padding: 3}}>
+    <CssVarsProvider
+         // the element to apply [data-joy-color-scheme] attribute.
+         colorSchemeNode={node || null}
+         //
+         // the selector to apply the CSS theme variables stylesheet.
+         colorSchemeSelector="#mode-toggle"
+         //
+         // the local storage key to use.
+         modeStorageKey="mode-toggle-demo"
+    >
+        <Box sx={{ display: 'flex', gap: 55, padding: 3}}>
       <Select defaultValue="Home">
         <Option value="login"><Link href='/' variant='outlined'><Typography level='h1'>Login</Typography></Link></Option>
         <Option value="dashboard"><Link href='/dashboard' variant='outlined'><Typography level='h1'>Dashboard</Typography></Link></Option>
@@ -24,10 +66,12 @@ const Header: React.FC = () => {
          NFT GATED SERVER
         </Typography>
       <ButtonGroup aria-label="outlined primary button group">
-      <Button>Light/Dark</Button>
+      <ModeSwitcher />
       <Button>Logout</Button>
     </ButtonGroup>
     </Box>
+    </CssVarsProvider>
+  
   );
 };
 
