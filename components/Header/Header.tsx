c8e048f21a92f784452f66f103cf90f53a3326ba
Achilles-Dev <solohag92@gmail.com>
2023-07-28 12:06:50 +0000

    Update Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import { Typography, Box, Select, Option, Button, ButtonGroup } from '@mui/joy';
 import ModeButton from '../ModeButton';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const Header: React.FC = () => {
-  const router = useRouter();
 
   return (
       <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: 2 }}>
         <Select
-          placeholder={router.pathname == '/' ? 'Home':  `${router.pathname.slice(1)[0].toUpperCase()}${router.pathname.slice(1).slice(1)}`}
+          placeholder='Home'
           slotProps={{
             listbox: {
               placement: 'top-start',
@@ -20,12 +18,12 @@ const Header: React.FC = () => {
             },
           }}
         >
-          <Option value="login"><Link href='/'><Typography level='h4'>Login</Typography></Link></Option>
-          <Option value="dashboard"><Link href='/dashboard'><Typography level='h4'>Dashboard</Typography></Link></Option>
-          <Option value="audience"><Link href='/audience'><Typography level='h4'>Address List</Typography></Link></Option>
-          <Option value="content"><Link href='/content'><Typography level='h4'>Content List</Typography></Link></Option>
-          <Option value="logs"><Link href='/logs'><Typography level='h4'>Logs</Typography></Link></Option>
-          <Option value="manage"><Link href='/manage'><Typography level='h4'>Manage</Typography></Link></Option>
+          <Link href='/'><Option value="login"><Typography level='h4'>Login</Typography></Option></Link>
+          <Link href='/dashboard'><Option value="dashboard"><Typography level='h4'>Dashboard</Typography></Option></Link>
+          <Link href='/audience'><Option value="audience"><Typography level='h4'>Address List</Typography></Option></Link>
+          <Link href='/content'><Option value="content"><Typography level='h4'>Content List</Typography></Option></Link>
+          <Link href='/logs'><Option value="logs"><Typography level='h4'>Logs</Typography></Option></Link>
+          <Link href='/manage'><Option value="manage"><Typography level='h4'>Manage</Typography></Option></Link>
         </Select>
         <Typography level='h3'>
           NFT GATED SERVER
