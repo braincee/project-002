4611062a70bf07e5620944ecdae964c5f82f4fdf
braincee <kwesi938@gmail.com>
2023-08-07 18:25:43 +0000

    BarChart refactor

--- components/BarChart/index.tsx
+++ components/BarChart/index.tsx
@@ -1,66 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import React from 'react'
+import dynamic from 'next/dynamic'
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const SingleBarChart = dynamic(() => import('@mui/x-charts').then((module) => module.BarChart), {
+    ssr: false,
+})
 
-const BarChart = ({ mapData }: { mapData: any }) => {
-  const [chartData, setChartData] = useState<any>({
-    labels: [],
-    datasets: [{}],
-  });
-  const { labels, dataValues } = mapData();
-  const [chartOptions, setChartOptions] = useState({});
 
-  useEffect(() => {
-    setChartData({
-      labels: labels,
-      datasets: [
-        {
-          label: "Daily Requests",
-          data: dataValues,
-          borderColor: "rgb(53, 162, 235)",
-          backgroundColor: "rgb(53, 162, 235, 0.4",
-        },
-      ],
-    });
-    setChartOptions({
-      plugins: {
-        legend: {
-          position: "top",
-        },
-        title: {
-          display: true,
-          text: "Address List",
-        },
-      },
-      maintainAspectRatio: false,
-      responsive: true,
-    });
-  }, []);
+const BarChart = () => {
+    return (
+        <>
+            <SingleBarChart
+                xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                width={500}
+                height={300}
+            />
+        </>
+    )
+}
 
-  return (
-    <>
-      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
-        <Bar data={chartData} options={chartOptions} />
-      </div>
-    </>
-  );
-};
-
-export default BarChart;
+export default BarChart
