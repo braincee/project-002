67604909f1f1900d29a5b75617d5eba2370d0485
Achilles-Dev <solohag92@gmail.com>
2023-08-08 12:49:28 +0000

    Change Tablein audience page

---
+++ components/TableHead/index.tsx
@@ -0,0 +1,171 @@
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { Box, Checkbox, Link } from "@mui/joy";
+import { visuallyHidden } from "@mui/utils";
+
+interface Data {
+  id: string;
+  address?: string;
+  title?: string;
+  description?: string;
+  Contents?: any[];
+  Addresses?: any[];
+  created_at: Date;
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "address",
+    numeric: false,
+    disablePadding: true,
+    label: "Address",
+  },
+  {
+    id: "Contents",
+    numeric: false,
+    disablePadding: false,
+    label: "Content Access",
+  },
+  {
+    id: "created_at",
+    numeric: false,
+    disablePadding: false,
+    label: "Date Created",
+  },
+];
+
+const headCells2: readonly HeadCell[] = [
+  {
+    id: "title",
+    numeric: false,
+    disablePadding: true,
+    label: "Title",
+  },
+  {
+    id: "description",
+    numeric: false,
+    disablePadding: true,
+    label: "Description",
+  },
+  {
+    id: "Addresses",
+    numeric: false,
+    disablePadding: false,
+    label: "Address Access",
+  },
+  {
+    id: "created_at",
+    numeric: false,
+    disablePadding: false,
+    label: "Date Created",
+  },
+];
+
+type Order = "asc" | "desc";
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+const TableHead = (props: EnhancedTableProps) => {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler =
+    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <thead>
+      <tr>
+        <th>
+          <Checkbox
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            slotProps={{
+              input: {
+                "aria-label": "select all desserts",
+              },
+            }}
+            sx={{ verticalAlign: "sub" }}
+          />
+        </th>
+        {headCells.map((headCell) => {
+          const active = orderBy === headCell.id;
+          return (
+            <th
+              key={headCell.id}
+              aria-sort={
+                active
+                  ? ({ asc: "ascending", desc: "descending" } as const)[order]
+                  : undefined
+              }
+            >
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link
+                underline="none"
+                color="neutral"
+                textColor={active ? "primary.plainColor" : undefined}
+                component="button"
+                onClick={createSortHandler(headCell.id)}
+                fontWeight="lg"
+                startDecorator={
+                  headCell.numeric ? (
+                    <ArrowDownwardIcon sx={{ opacity: active ? 1 : 0 }} />
+                  ) : null
+                }
+                endDecorator={
+                  !headCell.numeric ? (
+                    <ArrowDownwardIcon sx={{ opacity: active ? 1 : 0 }} />
+                  ) : null
+                }
+                sx={{
+                  "& svg": {
+                    transition: "0.2s",
+                    transform:
+                      active && order === "desc"
+                        ? "rotate(0deg)"
+                        : "rotate(180deg)",
+                  },
+                  "&:hover": { "& svg": { opacity: 1 } },
+                }}
+              >
+                {headCell.label}
+                {active ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === "desc"
+                      ? "sorted descending"
+                      : "sorted ascending"}
+                  </Box>
+                ) : null}
+              </Link>
+            </th>
+          );
+        })}
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHead;
--- pages/audience/index.tsx
+++ pages/audience/index.tsx
@@ -2,7 +2,12 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControl,
+  FormLabel,
+  IconButton,
   Input,
+  Option,
+  Select,
   Sheet,
   Stack,
   Table,
@@ -19,6 +24,11 @@ import {
 } from "@/libs/api";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { Address, Content } from "@/libs/models";
+import TableHead from "@/components/TableHead";
+import {
+  KeyboardArrowLeft as KeyboardArrowLeftIcon,
+  KeyboardArrowRight as KeyboardArrowRightIcon,
+} from "@mui/icons-material";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const addresses = JSON.stringify(
@@ -39,15 +49,29 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 };
 
+interface Data {
+  id: string;
+  address?: string;
+  title?: string;
+  description?: string;
+  Contents?: any[];
+  Addresses?: any[];
+  created_at: Date;
+}
+
 const AddressList = ({
   addresses,
   contentItems,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const [selected, setSelected] = useState<readonly string[]>([]);
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<keyof Data>("Contents");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   const [open, setOpen] = useState(false);
   const [disable, setDisable] = useState(false);
   const [selectedOption, setSelectedOption] = useState<string | null>("");
-  const [addressList, setAddressList] = useState(addresses);
+  const [addressList, setAddressList] = useState<any[]>(addresses);
 
   const isSelected = (index: string) => selected.indexOf(index) !== -1;
 
@@ -61,6 +85,15 @@ const AddressList = ({
         setDisable(false);
       });
     });
+  };
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
   };
 
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -92,6 +125,28 @@ const AddressList = ({
     setSelected(newSelected);
   };
 
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any, newValue: number | null) => {
+    setRowsPerPage(parseInt(newValue!.toString(), 10));
+    setPage(0);
+  };
+
+  const getLabelDisplayedRowsTo = () => {
+    if (addressList.length === -1) {
+      return (page + 1) * rowsPerPage;
+    }
+    return rowsPerPage === -1
+      ? addressList.length
+      : Math.min(addressList.length, (page + 1) * rowsPerPage);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - addressList.length) : 0;
+
   const handleContentAccess = () => {
     setOpen(true);
   };
@@ -122,11 +177,60 @@ const AddressList = ({
     });
   };
 
+  const labelDisplayedRows = ({
+    from,
+    to,
+    count,
+  }: {
+    from: number;
+    to: number;
+    count: number;
+  }) => {
+    return `${from}–${to} of ${count !== -1 ? count : `more than ${to}`}`;
+  };
+
+  const descendingComparator = <T extends {}>(a: T, b: T, orderBy: keyof T) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  type Order = "asc" | "desc";
+
+  const getComparator = <Key extends keyof any>(
+    order: Order,
+    orderBy: Key
+  ): ((
+    a: { [key in Key]: number | string },
+    b: { [key in Key]: number | string }
+  ) => number) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const stableSort = <T extends {}>(
+    array: readonly T[],
+    comparator: (a: T, b: T) => number
+  ) => {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
   return (
     <Box sx={{ px: 4, display: "flex", flexDirection: "column", gap: 2 }}>
-      <Typography level="h3">Address List</Typography>
-
-      <form
+      {/* <form
         onSubmit={(event) => {
           event.preventDefault();
           handleSubmit(event);
@@ -144,33 +248,38 @@ const AddressList = ({
             </Button>
           </Stack>
         </Stack>
-      </form>
+      </form> */}
 
       <Stack spacing={2}>
-        <Typography level="h4" sx={{ textAlign: "end" }}>
+        {/* <Typography level="h4" sx={{ textAlign: "end" }}>
           Total Number of Addresses: {addressList.length}{" "}
-        </Typography>
-        <Sheet sx={{ height: 400, overflow: "auto" }}>
+        </Typography> */}
+        <Sheet
+          variant="outlined"
+          sx={{ width: "100%", boxShadow: "sm", borderRadius: "sm" }}
+        >
           <TableToolbar
             numSelected={selected.length}
             handleAccess={handleContentAccess}
             buttonName={"Content"}
-            tableHeader={"Addresses"}
+            tableHeader={"Address List"}
           />
           <Table
             aria-label="stripe table"
             stripe="even"
-            stickyHeader
             hoverRow
             sx={{
               "--TableCell-headBackground": "transparent",
               "--TableCell-selectedBackground": (theme) =>
                 theme.vars.palette.primary.softBg,
               "& thead th:nth-child(1)": {
-                width: "40px",
+                width: "5%",
+              },
+              "& thead th:nth-child(2)": {
+                width: "55%",
               },
               "& thead th:nth-child(3)": {
-                width: "10%",
+                width: "20%",
               },
               "& thead th:nth-child(4)": {
                 width: "20%",
@@ -178,60 +287,136 @@ const AddressList = ({
               "& tr > *:nth-child(n+3)": { textAlign: "center" },
             }}
           >
-            <thead>
-              <tr>
-                <th>
-                  <Checkbox
-                    onChange={handleSelectAllClick}
-                    sx={{ verticalAlign: "sub" }}
-                  />
-                </th>
-                <th>Address</th>
-                <th>Content Access</th>
-                <th>Date Created</th>
-              </tr>
-            </thead>
+            <TableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={addressList.length}
+            />
             <tbody>
-              {addressList.map((address: any, index: number) => {
-                const isItemSelected = isSelected(address.id);
-                return (
-                  <tr
-                    key={index}
-                    onClick={(event) => handleClick(event, address.id)}
-                    role="checkbox"
-                    tabIndex={-1}
-                    aria-checked={isItemSelected}
-                    style={
-                      isItemSelected
-                        ? ({
-                            "--TableCell-dataBackground":
-                              "var(--TableCell-selectedBackground)",
-                            "--TableCell-headBackground":
-                              "var(--TableCell-selectedBackground)",
-                          } as React.CSSProperties)
-                        : {}
-                    }
-                  >
-                    <th scope="row">
-                      <Checkbox
-                        checked={isItemSelected}
-                        sx={{ verticalAlign: "top" }}
-                      />
-                    </th>
-                    <td>
-                      <Typography level="h4">{address.address}</Typography>
-                    </td>
-                    <td>{address.Contents.length}</td>
-                    <td>
-                      <Typography color="neutral">
-                        {new Date(address.created_at).toDateString()}
-                      </Typography>
-                    </td>
-                  </tr>
-                );
-              })}
-              <tr></tr>
+              {stableSort(addressList, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((address: any, index) => {
+                  const isItemSelected = isSelected(address.id.toString());
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <tr
+                      onClick={(event) =>
+                        handleClick(event, address.id.toString())
+                      }
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={address.id}
+                      // selected={isItemSelected}
+                      style={
+                        isItemSelected
+                          ? ({
+                              "--TableCell-dataBackground":
+                                "var(--TableCell-selectedBackground)",
+                              "--TableCell-headBackground":
+                                "var(--TableCell-selectedBackground)",
+                            } as React.CSSProperties)
+                          : {}
+                      }
+                    >
+                      <th scope="row">
+                        <Checkbox
+                          checked={isItemSelected}
+                          slotProps={{
+                            input: {
+                              "aria-labelledby": labelId,
+                            },
+                          }}
+                          sx={{ verticalAlign: "top" }}
+                        />
+                      </th>
+                      <th id={labelId} scope="row">
+                        {address.address}
+                      </th>
+                      <td>{address.Contents.length}</td>
+                      <td>{new Date(address.created_at).toDateString()}</td>
+                    </tr>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <tr
+                  style={
+                    {
+                      height: `calc(${emptyRows} * 40px)`,
+                      "--TableRow-hoverBackground": "transparent",
+                    } as React.CSSProperties
+                  }
+                >
+                  <td colSpan={6} />
+                </tr>
+              )}
             </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={6}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 2,
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <FormControl orientation="horizontal" size="sm">
+                      <FormLabel>Rows per page:</FormLabel>
+                      <Select
+                        onChange={handleChangeRowsPerPage}
+                        value={rowsPerPage}
+                      >
+                        <Option value={5}>5</Option>
+                        <Option value={10}>10</Option>
+                        <Option value={25}>25</Option>
+                      </Select>
+                    </FormControl>
+                    <Typography textAlign="center" sx={{ minWidth: 80 }}>
+                      {labelDisplayedRows({
+                        from:
+                          addressList.length === 0 ? 0 : page * rowsPerPage + 1,
+                        to: getLabelDisplayedRowsTo(),
+                        count:
+                          addressList.length === -1 ? -1 : addressList.length,
+                      })}
+                    </Typography>
+                    <Box sx={{ display: "flex", gap: 1 }}>
+                      <IconButton
+                        size="sm"
+                        color="neutral"
+                        variant="outlined"
+                        disabled={page === 0}
+                        onClick={() => handleChangePage(page - 1)}
+                        sx={{ bgcolor: "background.surface" }}
+                      >
+                        <KeyboardArrowLeftIcon />
+                      </IconButton>
+                      <IconButton
+                        size="sm"
+                        color="neutral"
+                        variant="outlined"
+                        disabled={
+                          addressList.length !== -1
+                            ? page >=
+                              Math.ceil(addressList.length / rowsPerPage) - 1
+                            : false
+                        }
+                        onClick={() => handleChangePage(page + 1)}
+                        sx={{ bgcolor: "background.surface" }}
+                      >
+                        <KeyboardArrowRightIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </td>
+              </tr>
+            </tfoot>
           </Table>
         </Sheet>
         <MyModal
