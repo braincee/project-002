87e0f03ec271651f906642b0147ff193e0db59a1
braincee <kwesi938@gmail.com>
2023-08-23 14:19:15 +0000

    Add rendered content logic

--- pages/serve/[contentId]/index.tsx
+++ pages/serve/[contentId]/index.tsx
@@ -21,7 +21,32 @@ const ServeContent = ({
   content,
   fileType,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  console.log(fileType);
+  let renderedContent;
+
+  if (fileType.startsWith("video")) {
+    renderedContent = (
+      <video controls src={content.url} sx={{ maxWidth: "100%" }} />
+    );
+  } else if (fileType.startsWith("image")) {
+    renderedContent = (
+      <img
+        src={content.url}
+        alt="Content"
+        sx={{ maxWidth: "100%", height: "auto" }}
+      />
+    );
+  } else if (fileType.startsWith("audio")) {
+    renderedContent = <audio controls src={content.url} />;
+  } else if (fileType === "link") {
+    renderedContent = (
+      <a href={content.url} target="_blank" rel="noopener noreferrer">
+        Link to Content
+      </a>
+    );
+  } else {
+    renderedContent = <p>Unsupported Content Type</p>;
+  }
+
   return (
     <>
       <Head>
@@ -29,7 +54,11 @@ const ServeContent = ({
         <meta name="robots" content="follow, index" />
         <meta name="description" content="description" />
       </Head>
-      <Box>Content</Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box sx={{ width: "100%", height: "100%", p: 10 }}>
+          {renderedContent}
+        </Box>
+      </Box>
     </>
   );
 };
