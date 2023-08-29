4e288021000183cf59920fc2f6bf31398e5a1402
braincee <kwesi938@gmail.com>
2023-08-29 15:19:53 +0000

    Add types to DragDrop

--- components/DragDrop/index.tsx
+++ components/DragDrop/index.tsx
@@ -1,34 +1,38 @@
-import { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
+import { useCallback, useState } from 'react';
+import { useDropzone, FileWithPath } from 'react-dropzone';
 
-//! Change this any type to fn
-const DragDrop = ({ setFile, checked }: { setFile: any; checked: boolean }) => {
-    const [display, setDisplay] = useState<any>(
-        "Drag 'n' drop some files here, or click to select files",
-    )
-
-    //! Change this any type, dropzone should have types in case they're not simple arrays
-    const onDrop = useCallback((acceptedFiles: any) => {
-        acceptedFiles.forEach((file: any) => {
-            setFile(file)
-            setDisplay(file.path)
-        })
-    }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-
-    return (
-        <div
-            {...getRootProps()}
-            style={{
-                border: '1px solid #cdd7e1',
-                padding: '0px 8px',
-                display: checked ? 'none' : 'block',
-            }}
-        >
-            <input {...getInputProps()} required={checked ? false : true} />
-            {isDragActive ? <p>Drop the files here ...</p> : <p>{display}</p>}
-        </div>
-    )
+interface DragDropProps {
+  setFile: (file: FileWithPath) => void;
+  checked: boolean;
 }
 
-export default DragDrop
+const DragDrop: React.FC<DragDropProps> = ({ setFile, checked }) => {
+  const [display, setDisplay] = useState<string>(
+    "Drag 'n' drop some files here, or click to select files"
+  );
+
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    acceptedFiles.forEach((file: FileWithPath) => {
+      setFile(file);
+      setDisplay(file.path);
+    });
+  }, [setFile]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      style={{
+        border: '1px solid #cdd7e1',
+        padding: '0px 8px',
+        display: checked ? 'none' : 'block',
+      }}
+    >
+      <input {...getInputProps()} required={!checked} />
+      {isDragActive ? <p>Drop the files here ...</p> : <p>{display}</p>}
+    </div>
+  );
+};
+
+export default DragDrop;
